f46c8ec1f6253c8c14b55e43105324f605cf49ca
Joshua <olakunmiigboro@gmail.com>
2022-10-13 00:30:09 -0700

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  *main - Printing in C
+ *
+ *   *
+ *
+ *    *Return:Always 0 (It was successful)
+ *
+ *     */
+
+int main (void)
+
+{
+
+	      puts("\"Programming is like building a multilingual
+
+			      puzzle");
+
+	            return (0);
+
+}
