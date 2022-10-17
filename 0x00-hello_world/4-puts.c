7568d0fbecb0c3ee7d60baea94de34a78d2c3b2c
Joshua <olakunmiigboro@gmail.com>
2022-10-17 12:59:53 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,24 +1,33 @@
-
 #include <stdio.h>
+
+#include <string.h>
 
 
 
 /**
+ * *main - Main function of my project
  *
- *  main - Printing
+ * *
  *
- * 
+ * *
  *
- *  Return: Always 0 (Successful)
+ * *Return: return 0 everytime
  *
- * /
-
+ */
 int main(void)
 
 {
 
-	puts("\"Programming is like building a multilingual puzzle");
 
-	return (0);
+
+		char displayed[52];
+
+
+
+		strcpy(displayed, "\"Programming is like building a multilingual puzzle");
+
+		puts(displayed);
+
+		return (0);
 
 }
